4ca3bb9f7af41098c01bab69228cc144f8b22440
Yotam <yotamyizhar@gmail.com>
2021-01-09 17:32:25 +0200

    Added Working armor and the Ruby Ore block

--- src/main/java/com/YutyTuty/tutorial/core/init/BlockInit.java
+++ src/main/java/com/YutyTuty/tutorial/core/init/BlockInit.java
@@ -11,6 +11,7 @@ import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+
 public class BlockInit {
 	public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,
 			TutorialMod.MOD_ID);
